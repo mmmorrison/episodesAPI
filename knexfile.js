2db344116dac23a2b44484bfdd2e17df3bfa80d9
Maria <mmorrison1978@gmail.com>
2016-04-28 16:53:06 -0600

    added dotenv

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,5 @@
+require('dotenv').load();
+
 module.exports = {
 
   development: {
@@ -7,7 +9,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL
+    connection: process.env.DATABASE_URL + '?ssl=true'
   }
 
 };
